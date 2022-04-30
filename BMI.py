4d00ebecbe178038f95775536078f8990c223fba
kokooshi <103607535+kokooshi@users.noreply.github.com>
2022-04-30 13:54:52 -0700

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -1,1 +1,17 @@
+#BMI = weight / height**2
+#result= whole number
 
+height = input("enter height in m: ")
+weight = input("enter weight in kg: ")
+
+#BMI = weight/height**2
+#print (BMI)
+#Error : unsupported operand type(s) for ** or pow(): 'str' and 'int'
+
+a= int (weight)
+b= float (height)
+BMI= int(a)/ float (b)**2
+#print (BMI)
+#result still not the whole number 
+result= int (BMI)
+print(result)
